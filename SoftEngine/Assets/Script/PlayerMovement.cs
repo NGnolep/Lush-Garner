4286e531094f8a9caf35990f906324957568760a
Bryant Chandra <bryantchandra2806@gmail.com>
2025-05-09 16:15:28 +0700

    Character Anim Implemention

--- SoftEngine/Assets/Script/PlayerMovement.cs
+++ SoftEngine/Assets/Script/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +19,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = 5f;
-        float moveX = Input.GetAxis("Horizontal"); 
-        float moveY = Input.GetAxis("Vertical");   
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        transform.position += new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
