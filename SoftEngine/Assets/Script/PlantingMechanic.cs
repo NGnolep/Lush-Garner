49479c749116fb15a04719bbcfd2406566e5d2a7
NhoelGoei <goeinhoel@gmail.com>
2025-05-18 20:20:25 +0700

    added some feature to the planting
    keluarin e to plant

--- SoftEngine/Assets/Script/PlantingMechanic.cs
+++ SoftEngine/Assets/Script/PlantingMechanic.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using TMPro;
 
 public class PlantingMechanics : MonoBehaviour
 {
     public Tilemap groundTilemap;
     public TileBase plantedTile;
-
+    public GameObject eText;
     void Update()
     {
+        ShowOrHideEText();
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             TryPlant();
         }
     }
 
+    void ShowOrHideEText()
+    {
+        Vector3Int cellPos = groundTilemap.WorldToCell(transform.position);
+        TileBase currentTile = groundTilemap.GetTile(cellPos);
+
+        if (IsUnplantedDirt(currentTile))
+        {
+            eText.SetActive(true);
+        }
+        else
+        {
+            eText.SetActive(false);
+        }
+    }
     void TryPlant()
     {
         Vector3Int cellPos = groundTilemap.WorldToCell(transform.position);
