3d403f56b3f2181206b6faa8c31f5e55d669a903
Bryant Chandra <bryantchandra2806@gmail.com>
2025-05-22 15:54:40 +0700

    Update shop not final

--- SoftEngine/Assets/Script/PlayerMovement.cs
+++ SoftEngine/Assets/Script/PlayerMovement.cs
@@ -13,17 +13,20 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
         animator.SetFloat("MoveX", movement.x);
         animator.SetFloat("MoveY", movement.y);
-
+    }
+    void FixedUpdate()
+    {
+        // Move in physics step
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
---
+++ SoftEngine/Assets/Script/Shop.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSystem : MonoBehaviour
+{
+    public GameObject interactPrompt;
+    public GameObject shopPanel;
+    public CanvasGroup shopCanvasGroup;
+
+    public float fadeDuration = 0.3f;
+
+    private bool isInTrigger = false;
+    private bool isShopOpen = false;
+
+    void Update()
+    {
+        if (isInTrigger)
+        {
+            interactPrompt.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.F) && !isShopOpen)
+            {
+                StartCoroutine(OpenShop());
+            }
+        }
+        else
+        {
+            interactPrompt.SetActive(false);
+        }
+
+        if (isShopOpen && Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(CloseShop());
+        }
+    }
+
+    public void ClosingShopUI()
+    {
+        StartCoroutine(CloseShop());
+    }
+    IEnumerator OpenShop()
+    {
+        shopPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isShopOpen = true;
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            shopCanvasGroup.alpha = Mathf.Lerp(0f, 1f, elapsed / fadeDuration);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        shopCanvasGroup.alpha = 1f;
+    }
+
+    IEnumerator CloseShop()
+    {
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            shopCanvasGroup.alpha = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        shopCanvasGroup.alpha = 0f;
+
+        shopPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isShopOpen = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTrigger = false;
+        }
+    }
+}
